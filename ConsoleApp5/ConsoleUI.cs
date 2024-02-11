0ca86a52766aa93c94b012a95b45ada2a9e2f132
William <william.bjorklund.00@gmail.com>
2024-02-11 22:42:33 +0100

    Task is finished

---
+++ ConsoleApp5/ConsoleUI.cs
@@ -0,0 +1,94 @@
+﻿using ConsoleApp5.Services;
+
+namespace ConsoleApp5
+{
+    internal class ConsoleUI
+    {
+        private readonly ProductService _productService;
+
+        public ConsoleUI(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // PRODUCTS
+        public void CreateProduct_UI()
+        {
+            Console.Clear();
+            Console.WriteLine("---- CREATE PRODUCT ----");
+
+            Console.Write("Product Title: ");
+            var title = Console.ReadLine()!;
+
+            Console.Write("Product Price: ");
+            var price = decimal.Parse(Console.ReadLine()!);
+
+            Console.Write("Product Brand: ");
+            var brandName = Console.ReadLine()!;
+
+            var result = _productService.CreateProduct(title, price, brandName);
+            if (result != null)
+            {
+                Console.Clear();
+                Console.WriteLine("Product was created.");
+                Console.ReadKey();
+            }
+        }
+        public void GetProducts_UI()
+        {
+            Console.Clear();
+
+            var products = _productService.GetProducts();
+            foreach (var product in products)
+            {
+                Console.WriteLine($"{product.Name} - {product.Brand} ({product.Price} SEK)");
+            }
+
+            Console.ReadKey();
+        }
+        public void UpdateProduct_UI()
+        {
+            Console.Clear();
+            Console.Write("Enter Product Id: ");
+            var id = int.Parse(Console.ReadLine()!);
+
+            var product = _productService.GetProductById(id);
+            if (product != null)
+            {
+                Console.WriteLine($"{product.Name} - {product.Brand} ({product.Price} SEK)");
+                Console.WriteLine();
+
+                Console.Write("New Product Title: ");
+                product.Name = Console.ReadLine()!;
+
+                var newProduct = _productService.UpdateProduct(product);
+                Console.WriteLine($"{product.Name} - {product.Brand} ({product.Price} SEK)");
+            }
+            else
+            {
+                Console.WriteLine("No product found.");
+            }
+
+            Console.ReadKey();
+        }
+        public void DeleteProduct_UI()
+        {
+            Console.Clear();
+            Console.Write("Enter Product Id: ");
+            var id = int.Parse(Console.ReadLine()!);
+
+            var product = _productService.GetProductById(id);
+            if (product != null)
+            {
+                _productService.DeleteProduct(id);
+                Console.WriteLine("Product was deleted");
+            }
+            else
+            {
+                Console.WriteLine("No product found.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,4 +1,7 @@
-﻿using ConsoleApp5.Contexts;
+﻿using ConsoleApp5;
+using ConsoleApp5.Contexts;
+using ConsoleApp5.Repositories;
+using ConsoleApp5.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,4 +11,24 @@ var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
 
     services.AddDbContext<DataContext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\willi\OneDrive\Skrivbord\Datalagring\ConsoleApp5\ConsoleApp5\Data\database.mdf;Integrated Security=True;Connect Timeout=30"));
 
+    services.AddScoped<AddressRepository>();
+    services.AddScoped<BrandRepository>();
+    services.AddScoped<CustomerRepository>();
+    services.AddScoped<ProductRepository>();
+    services.AddScoped<ProductReviewRepository>();
+    services.AddScoped<AddressService>();
+    services.AddScoped<BrandService>();
+    services.AddScoped<CustomerService>();
+    services.AddScoped<ProductService>();
+    services.AddScoped<ProductReviewService>();
+
+    services.AddSingleton<ConsoleUI>();
+
 }).Build();
+
+var consoleUI = builder.Services.GetRequiredService<ConsoleUI>();
+
+consoleUI.CreateProduct_UI();
+consoleUI.GetProducts_UI();
+consoleUI.UpdateProduct_UI();
+consoleUI.DeleteProduct_UI();
